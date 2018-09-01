69b9130b0cc6fddae4709a08f019c3a2c9b54eda
Faidon Liambotis <faidon@wikimedia.org>
2018-09-01 17:40:53 +0300

    Add pytest support for unit/integration testing
    Plus one example test case for --help.
    
    Change-Id: I8116f480f98a70a9e04995723a81a1463631a1a1

---
+++ keyholder/tests/unit/test_argparse.py
@@ -0,0 +1,14 @@
+"""Argument parse tests."""
+
+import keyholder.daemon
+import pytest
+
+
+def test_parse_args_help(capsys):
+    """Test whether --help returns usage and exits"""
+    with pytest.raises(SystemExit) as exc:
+        keyholder.daemon.parse_args(['--help'])
+
+    out, _ = capsys.readouterr()
+    assert exc.value.code == 0
+    assert 'usage: ' in out
