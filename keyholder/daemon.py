ffd7c8dedcd5458df1f28b769d54ec8817de13c8
Faidon Liambotis <faidon@wikimedia.org>
2018-09-04 20:40:58 +0300

    Stop spawning ssh-keygen but generate fps ourselves
    OpenSSH's public key format is really trivial, so parse it ourselves
    rather than spawning ssh-keygen -lf.
    
    Change-Id: I0f68f6a839a9b7dbbc09dad6466c4ba6c6fcae4f

--- keyholder/daemon.py
+++ keyholder/daemon.py
@@ -23,6 +23,7 @@
 """
 import argparse
 import base64
+import binascii
 import collections
 import ctypes
 import grp
@@ -34,7 +35,6 @@ import pwd
 import socket
 import socketserver
 import struct
-import subprocess
 import sys
 import yaml
 
@@ -63,11 +63,13 @@ def get_key_fingerprints(key_dir):
         logger.warning('%s is not a directory', key_dir)
 
     for fname in key_dir.glob('*.pub'):
-        line = subprocess.check_output(
-            ['/usr/bin/ssh-keygen', '-lf', str(fname)],
-            universal_newlines=True)
-        _, fingerprint, _ = line.split(' ', 2)
-        keymap[fname.stem] = fingerprint
+        _, key_blob64, _ = fname.read_text().split()
+        try:
+            key_blob = base64.b64decode(key_blob64)
+        except binascii.Error:
+            logger.warning('Could not parse key %s', str(fname))
+            continue
+        keymap[fname.stem] = ssh_fingerprint(key_blob)
     logger.info('Successfully loaded %d key(s)', len(keymap))
     return keymap
 
