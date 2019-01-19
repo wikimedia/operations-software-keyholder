a588fd6bfc0518b3805545a0cad41fffdddc62a1
Faidon Liambotis <faidon@wikimedia.org>
2019-01-19 17:44:00 +0200

    vyaaaaaaaaaaaa

--- keyholder/tests/test_argparse.py
+++ keyholder/tests/test_argparse.py
@@ -7,8 +7,8 @@ import keyholder.daemon
 def test_parse_args_help(capsys):
     """Test whether --help returns usage and exits"""
     with pytest.raises(SystemExit) as exc:
-        keyholder.daemon.parse_args(['--help'])
+        keyholder.daemon.parse_args(["--help"])
 
     out, _ = capsys.readouterr()
     assert exc.value.code == 0
-    assert 'usage: ' in out
+    assert "usage: " in out
